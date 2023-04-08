8497e19431ea277a2f013b64c08b30ccdf3f544f
Alaire <89337095+Alaire1@users.noreply.github.com>
2023-04-08 19:15:57 +0200

    Create ft_print_unsigned.c

---
+++ ft_print_unsigned.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_unsigned.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akaraban <akaraban@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 18:09:25 by akaraban          #+#    #+#             */
+/*   Updated: 2023/03/14 02:45:36 by akaraban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	print_nbr(unsigned int nbr)
+{
+	if (nbr > 9)
+		ft_print_unsigned(nbr / 10);
+	if (nbr <= 9)
+	{
+		ft_putchar(nbr + 48);
+		return ;
+	}
+	ft_putchar((nbr % 10) + 48);
+}
+
+int	ft_print_unsigned(unsigned int nbr)
+{
+	unsigned int	i;
+
+	i = 1;
+	print_nbr(nbr);
+	while (nbr > 9)
+	{
+		nbr = nbr / 10;
+		i++;
+	}
+	return (i);
+}
